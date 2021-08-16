75f012d13c57718c52a583aa2fa5c6340aecbf0b
yusuf khan <patanyusufkhan222@gmail.com>
2021-08-16 20:08:51 +0530

    jdsgf

--- API/models/address.model.js
+++ API/models/address.model.js
@@ -10,7 +10,7 @@ const addressSchema = mongoose.Schema({
     fullname:{type:String,required:true},
     addressline1:{type:String,required:true},
     addressline2:{type:String,required:true},
-    landmark:{type:String,required:true},
+    landmark1:{type:String,required:true},
     city:{type:String,required:true},
     state:{type:String,required:true},
     pin:{type:String,required:true},
